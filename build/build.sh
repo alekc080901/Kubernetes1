78a319c1f54feb381b7cf2217d746626f4a78fd3
Alexander Nikitin <nikitinfo@ya.ru>
2025-05-20 01:01:50 +0300

    Add destionation rule

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,8 @@
 mvnw package -Dmaven.test.skip=true
 docker build -t server -f .\build\Dockerfile .
+
+kubectl label namespace default istio-injection=enabled
+
 kubectl apply -f .\build\ConfigMap.yaml
 kubectl apply -f .\build\Pod.yaml
 kubectl apply -f .\build\Deployment.yaml
--- src/main/java/ru/mipt/home/BeanConfiguration.java
+++ src/main/java/ru/mipt/home/BeanConfiguration.java
@@ -1,8 +1,12 @@
 package ru.mipt.home;
 
 import jakarta.annotation.PostConstruct;
+import org.apache.catalina.connector.Connector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -24,5 +28,23 @@ public class BeanConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    @ConditionalOnProperty("server.ssl.enabled")
+    public ServletWebServerFactory servletContainer(@Value("${server.http.port}") int httpPort,
+                                                    @Value("${server.port}") int httpsPort) {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createHttpConnector(httpPort, httpsPort));
+        return tomcat;
+    }
+
+    private Connector createHttpConnector(int httpPort, int httpsPort) {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(httpPort);
+        connector.setSecure(false);
+        connector.setScheme("http");
+        connector.setRedirectPort(httpsPort);
+        return connector;
     }
 }
